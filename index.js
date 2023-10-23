f60a20b08c225c4bf1b37677f4d7237d0f7e0669
llja <lesterlim.ja@gmail.com>
2023-10-24 07:11:40 +0800

    feat:added cors

--- index.js
+++ index.js
@@ -2,8 +2,12 @@ const express = require('express')
 const { getSightings } = require('./utils.js')
 require('dotenv').config()
 
+const cors= require('cors')
 const PORT = process.env.PORT;
 const app = express();
+
+//set up cors
+app.use(cors())
 
 app.get("/sightings", async (req, res) => {
   const sightings = await getSightings();
